8f3b02b4d798473b5d2447e0aeef8e05f73a48f1
Keji Li <mail@keji.li>
2026-07-20 17:00:59 -0400

    feat: argument parsers for validation

--- medim_val_dataset.py
+++ medim_val_dataset.py
@@ -1,33 +1,59 @@
 # -*- encoding: utf-8 -*-
 
-import os.path as osp
-from glob import glob
+from typing import NamedTuple
+from sys import stdout
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
 
 import medim
 from tqdm import tqdm
 
 from utils.infer_utils import validate_paired_img_gt
 
+class Args(NamedTuple):
+    image: Path
+    groundtruth: Path
+    output: Path
+    checkpoint: Path
+
+def parse_args() -> Args:
+    parser = ArgumentParser()
+    parser.add_argument('--image', '-i', help='validation dataset image folder', type=str)
+    parser.add_argument('--groundtruth', '-g', help='validation dataset ground truth', type=str)
+    parser.add_argument('--output', '-o', help='output folder for prediction results', type=str)
+    parser.add_argument('--checkpoint', '-c', help='model checkpoint path', type=str)
+    args = parser.parse_args()
+    return Args(**{k: Path(v) for k, v in vars(args).items()})
+
+def get_logger(file_path: Path) -> logging.Logger:
+    log_format = logging.Formatter(
+        fmt="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    logger = logging.getLogger(__file__)
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler(stdout)
+    console_handler.setFormatter(log_format)
+    file_handler = logging.FileHandler(file_path, mode='a', encoding='utf-8')
+    file_handler.setFormatter(log_format)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    return logger
+
 if __name__ == "__main__":
     ''' prepare the pre-trained model with local path or huggingface url '''
-    # ckpt_path = "https://huggingface.co/blueyo0/SAM-Med3D/blob/main/sam_med3d_turbo.pth"
-    # or you can use a local path like:
-    # ckpt_path = "./ckpt/sam_med3d_turbo.pth"
-
-    test_data_list = [
-        dict(
-            img_dir="./data/ct_AMOS/imagesVal",
-            gt_dir="./data/ct_AMOS/labelsVal",
-            out_dir="./data/ct_AMOS/pred_sammed3d",
-            ckpt_path="./ckpt/sam_med3d_turbo.pth",
-        ),
-    ]
-    for test_data in test_data_list:
-        model = medim.create_model("SAM-Med3D", pretrained=True, checkpoint_path=test_data["ckpt_path"])
-        gt_fname_list = sorted(glob(osp.join(test_data["gt_dir"], "*.nii.gz")))
-        for gt_fname in tqdm(gt_fname_list):
-            case_name = osp.basename(gt_fname).replace(".nii.gz", "")
-            img_path = osp.join(test_data["img_dir"], f"{case_name}.nii.gz")
-            gt_path = gt_fname
-            out_path = osp.join(test_data["out_dir"], f"{case_name}.nii.gz")
+    args = parse_args()
+    model = medim.create_model("SAM-Med3D", pretrained=True, checkpoint_path=str(args.checkpoint))
+    img_files = set([x.name.replace('_0000.nii.gz', '') for x in args.image.glob("*.nii.gz")])
+    gt_files = set([x.name.replace('.nii.gz', '') for x in args.groundtruth.glob('*.nii.gz')])
+    logger = get_logger(Path('medim_val.log'))
+    for case_name in tqdm(sorted(list(img_files & gt_files))):
+        print(f'[working] {case_name}')
+        img_path = args.image.joinpath(f"{case_name}_0000.nii.gz")
+        gt_path = args.groundtruth.joinpath(f"{case_name}.nii.gz")
+        out_path = args.output.joinpath(f"{case_name}.nii.gz")
+        try:
             validate_paired_img_gt(model, img_path, gt_path, out_path, num_clicks=1)
+        except RuntimeError as e:
+            logger.error(f'[error] {case_name} fail for:\n\t{str(e)}')
--- medim_val_single.py
+++ medim_val_single.py
@@ -1,31 +1,36 @@
 # -*- encoding: utf-8 -*-
 
+from typing import NamedTuple
+from argparse import ArgumentParser
+from pathlib import Path
 import medim
 
 from utils.infer_utils import validate_paired_img_gt
 from utils.metric_utils import compute_metrics, print_computed_metrics
 
-if __name__ == "__main__":
-    ''' 1. prepare the pre-trained model with local path or huggingface url '''
-    ckpt_path = "https://huggingface.co/blueyo0/SAM-Med3D/blob/main/sam_med3d_turbo.pth"
-    # or you can use a local path like:
-    model = medim.create_model("SAM-Med3D", pretrained=True, checkpoint_path=ckpt_path)
+class Args(NamedTuple):
+    image: Path
+    groundtruth: Path
+    output: Path
+    checkpoint: Path
 
-    ''' 2. read and pre-process your input data '''
-    img_path = "./test_data/amos_val_toy_data/imagesVa/amos_0013.nii.gz"
-    gt_path = "./test_data/amos_val_toy_data/labelsVa/amos_0013.nii.gz"
-    out_path = "./test_data/amos_val_toy_data/pred/amos_0013.nii.gz"
-    
-    ''' 3. infer with the pre-trained SAM-Med3D model '''
+def parse_args() -> Args:
+    parser = ArgumentParser()
+    parser.add_argument('--image', '-i', help='validation images', type=str)
+    parser.add_argument('--groundtruth', '-g', help='validation ground truth label', type=str)
+    parser.add_argument('--output', '-o', help='output label file path prediction results', type=str)
+    parser.add_argument('--checkpoint', '-c', help='checkpoint path', type=str)
+    args = parser.parse_args()
+    return Args(**{k: Path(v) for k, v in vars(args).items()})
+
+if __name__ == "__main__":
+    args = parse_args()
+    model = medim.create_model("SAM-Med3D", pretrained=True, checkpoint_path=str(args.checkpoint))
+
     print("Validation start! plz wait for some times.")
-    validate_paired_img_gt(model, img_path, gt_path, out_path, num_clicks=1)
+    validate_paired_img_gt(model, args.image, args.groundtruth, args.output, num_clicks=1)
     print("Validation finish! plz check your prediction.")
 
     ''' 4. compute the metrics of your prediction with the ground truth '''
-    metrics = compute_metrics(
-        gt_path=gt_path,
-        pred_path=out_path,
-        metrics=['dice'],
-        classes=None,
-    )
+    metrics = compute_metrics(str(args.groundtruth), str(args.output), None, ['dice'])
     print_computed_metrics(metrics)
